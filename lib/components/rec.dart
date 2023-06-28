fec595af496f0e10a08080570d02682abe480176
Diya Baiju N <diya_20b142cs@gecwyd.ac.in>
2023-06-28 22:39:16 +0530

    added offline speech to text

--- lib/components/rec.dart
+++ lib/components/rec.dart
@@ -9,6 +9,7 @@ import 'package:flutter_audio_recorder2/flutter_audio_recorder2.dart';
 import 'package:get/get.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:http/http.dart' as http;
+import 'package:speech_to_text/speech_to_text.dart';
 
 import 'bart_payload.dart';
 
@@ -17,116 +18,105 @@ class Rec {
 
   final BuildContext context;
   final HomeController homeController = Get.find();
+  SpeechToText speech = SpeechToText();
+
   bool _isRec = false;
   late Uint8List contents;
   late final FlutterAudioRecorder2 recorder;
-
+  late bool available;
   final DbModule dbModule = DbModule();
 
   //Todo: add init function
   initRecorder() async {
-    Directory tempDir = await getTemporaryDirectory();
-    String tempPath = tempDir.path;
-    if (File("$tempPath/myfile.aac").existsSync()) {
-      File("$tempPath/myfile.aac").deleteSync();
-    }
-    recorder = FlutterAudioRecorder2("$tempPath/myfile.aac",
-        audioFormat: AudioFormat.AAC); // .wav .aac .m4a
+    available =
+        await speech.initialize(onStatus: (status) {}, onError: (result) {});
+
+    // Directory tempDir = await getTemporaryDirectory();
+    // String tempPath = tempDir.path;
+    // if (File("$tempPath/myfile.aac").existsSync()) {
+    //   File("$tempPath/myfile.aac").deleteSync();
+    // }
+    // recorder = FlutterAudioRecorder2("$tempPath/myfile.aac",
+    //     audioFormat: AudioFormat.AAC); // .wav .aac .m4a
     dbModule.initDB();
   }
 
   recordMic() async {
     //then async await
     _isRec = !(_isRec);
-    print("foobar");
-    bool? isPermitted = await FlutterAudioRecorder2.hasPermissions;
-    print("permission: $isPermitted");
-    if (isPermitted ?? false) {
-      print("Staring record");
-      await recorder.initialized;
-      if (_isRec) {
-        print("Recording");
-        await recorder.start();
-        homeController.updateIsQueryReady(false);
-        homeController.isAnswerReady(false);
-        //print('tempPath/myFile.m4a');
+    print("Staring record");
+    if (_isRec) {
+      print("Recording");
+      if (available) {
+        speech.listen(onResult: (result) {
+          homeController.updateUserPrompt(result.recognizedWords);
+        });
+      } else {
+        print("speech recognition not available.");
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("speech recognition not available")));
       }
-      if (!_isRec) {
-        var recording = await recorder.stop();
-        print("stopping rec..");
-        if (recording?.path != null) {
-          print(recording!.path!);
-          contents = await File(recording!.path!).readAsBytes();
-          // homeController.updateIsQueryReady(true);
-          print("isQueryReady: ${homeController.isQueryReady.value}");
-          var url = Uri.parse(
-              "https://api-inference.huggingface.co/models/openai/whisper-large-v2");
-          var whisperRes = await http.post(url,
-              headers: {
-                "Authorization": "Bearer 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"
-              },
-              body: contents);
-          print(whisperRes.body);
-          var resultBody = jsonDecode(whisperRes.body);
-          var text = resultBody['text'] ?? '_';
-          // text = '_';
-          print(text);
-          if (text != '_') {
-            homeController.updateUserPrompt(text);
-            // var bartUrl = Uri.parse(
-            //     "https://api-inference.huggingface.co/models/facebook/bart-large-mnli");
-            var bartUrl = Uri.parse(
-                "https://api-inference.huggingface.co/models/MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli");
-            var payloadRaw = {
-              "inputs": "$text",
-              "parameters": {
-                "candidate_labels": intents,
-              }
-            };
-            var payload = jsonEncode(payloadRaw);
-            print(payload);
-            var bartRes = await http.post(bartUrl,
-                headers: {
-                  "Authorization":
-                      "Bearer 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"
-                },
-                body: payload);
+      // some time later...
 
-            print(bartRes.body);
-            Map<String, dynamic> bartBody = jsonDecode(bartRes.body);
-            if (bartBody.containsKey('error')) {
-              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                content: Text(
-                  "DeBERTa is being initialized",
-                  style: TextStyle(color: Colors.white),
-                ),
-                backgroundColor: blue,
-              ));
-              print("API Error: API is being initialized. Please wait");
-              // await dbModule.testDB();
-              return;
-            }
-            var bartpayload = BartPayload.fromJson(jsonDecode(bartRes.body));
-            var intentRes = bartpayload.labels;
-            print(intentRes);
-            if (intentRes != null) {
-              homeController.updateIntents(intentRes.sublist(0, 2));
-              await dbModule.getAnswerFromDB(intentRes.sublist(0, 2));
-              // dbModule.testDB();
-            }
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              content: Text(
-                "Whisper is being initialized. Try again in 4 Minutes.",
-                style: TextStyle(color: Colors.white),
-              ),
-              backgroundColor: blue,
-            ));
-            print("Whisper is being initialized");
+      // await recorder.start();
+      homeController.updateIsQueryReady(false);
+      homeController.isAnswerReady(false);
+      //print('tempPath/myFile.m4a');
+    }
+    if (!_isRec) {
+      print("stopping rec..");
+      speech.stop();
+      print(homeController.userPrompt.value);
+      if (homeController.userPrompt.value.isNotEmpty) {
+        // var bartUrl = Uri.parse(
+        //     "https://api-inference.huggingface.co/models/facebook/bart-large-mnli");
+        var bartUrl = Uri.parse(
+            "https://api-inference.huggingface.co/models/MoritzLaurer/DeBERTa-v3-base-mnli-fever-anli");
+        var payloadRaw = {
+          "inputs": homeController.userPrompt.value,
+          "parameters": {
+            "candidate_labels": intents,
           }
-        } else {
-          print("Whisper is unavailable");
+        };
+        var payload = jsonEncode(payloadRaw);
+        print(payload);
+        var bartRes = await http.post(bartUrl,
+            headers: {
+              "Authorization": "Bearer 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"
+            },
+            body: payload);
+
+        print(bartRes.body);
+        Map<String, dynamic> bartBody = jsonDecode(bartRes.body);
+        if (bartBody.containsKey('error')) {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text(
+              "DeBERTa is being initialized",
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: blue,
+          ));
+          print("API Error: API is being initialized. Please wait");
+          // await dbModule.testDB();
+          return;
         }
+        var bartpayload = BartPayload.fromJson(jsonDecode(bartRes.body));
+        var intentRes = bartpayload.labels;
+        print(intentRes);
+        if (intentRes != null) {
+          homeController.updateIntents(intentRes.sublist(0, 2));
+          await dbModule.getAnswerFromDB(intentRes.sublist(0, 2));
+          // dbModule.testDB();
+        }
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+            "Whisper is being initialized. Try again in 4 Minutes.",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: blue,
+        ));
+        print("Whisper is being initialized");
       }
     }
   }
